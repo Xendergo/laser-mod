81e223eb375bb5b2ab1fb06bb5708a915ac0204a
Xendergo <h.rovnyak@gmail.com>
2021-02-03 22:44:13 -0500

    Set frequency with comparator
    lol this took way too long, i am trash at java

--- src/main/java/net/fabricmc/LaserMod/blocks/Laser.java
+++ src/main/java/net/fabricmc/LaserMod/blocks/Laser.java
@@ -10,6 +10,14 @@ import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.Direction;
 
 public class Laser extends Block {
+  /* Using lambda for wavelength
+  [0-1): microwave
+  [1-2): infrared
+  [2-13): visible
+  [13-14): UV
+  [14-15): γ rays
+  */
+  public float λ = 7;
   public Laser() {
     super(FabricBlockSettings.of(Material.METAL).hardness(3.0f).resistance(1200.0f).nonOpaque());
     setDefaultState(this.stateManager.getDefaultState().with(Properties.FACING, Direction.NORTH));
@@ -22,5 +30,10 @@ public class Laser extends Block {
   @Override
   protected void appendProperties(StateManager.Builder<Block, BlockState> stateManager) {
     stateManager.add(Properties.FACING);
+  }
+
+  public void updateComparatorPower(int powerIn) {
+    λ = powerIn;
+    System.out.println(λ);
   }
 }
